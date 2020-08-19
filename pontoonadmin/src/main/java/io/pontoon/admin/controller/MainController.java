c8ae5f8ec6ea680f09f4f13c5afbfbee7def47d7
한승수 <seungsoo.han@samsung.com>
2020-08-19 18:42:25 +0900

    Web Templates Add

---
+++ pontoonadmin/src/main/java/io/pontoon/admin/controller/MainController.java
@@ -0,0 +1,19 @@
+package io.pontoon.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/")
+public class MainController {
+
+    public String home() {
+        return "index";
+    }
+}
